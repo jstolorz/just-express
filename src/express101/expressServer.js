f3186cb7be35d85d2c8242b9a0f1359fc8298d93
Janusz Stolorz <jstolorz@gmail.com>
2022-08-12 17:38:22 +0200

    Finish express simple http server

--- src/express101/expressServer.js
+++ src/express101/expressServer.js
@@ -1,1 +1,19 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+app.use(express.static('public'))
+
+app.get('/',(req,res) => {
+    res.sendFile('index.html')
+})
+
+app.all('*',(req,res) => {
+    res.status(404).send('<h2 style="color: red;">Page does not exist</h2>')
+})
+
+app.listen(3000)
+
+
 
