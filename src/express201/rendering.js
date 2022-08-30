6a471058168a984a9b3cf99b403498b985ee3a90
Janusz <jstolorz@gmail.com>
2022-08-30 17:11:33 +0200

    second example of template engine in express ejs hbs pug

--- src/express201/rendering.js
+++ src/express201/rendering.js
@@ -9,11 +9,34 @@ app.use(express.static('public'))
 app.use(express.json())
 app.use(express.urlencoded())
 
-app.set('view engine','ejs')
+app.set('view engine','pug')
 //app.set('views',path.join(__dirname,'views'))
 
+const validateUser = (req,res,next) => {
+   res.locals.validated = true
+   next()
+}
+
+app.use(validateUser)
+
 app.get('/abc',(req,res,next) => {
-   res.render('index')
+   res.render('index',{
+      country: {
+         name: 'Poland',
+         capital: 'Warsaw'
+      },
+      peoples: [{
+        name: 'janusz',
+        age: 53
+      },{
+         name: 'iwona',
+         age: 52
+      }],
+      msg: 'Success',
+      msg2: "Failure!",
+      html: `<h2>Hello from HTML</h2>
+            <p>sdsdsdsddsdsddsdssds</p>`
+   })
 })
 
 app.listen(4000)
